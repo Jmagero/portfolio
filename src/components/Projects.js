39f59bf18aa0a834af616a1dfb1cdbbbd7174373
jmagero <jmagero54@gmail.com>
2021-12-01 14:14:10 -0600

    Add project component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import Thumbnail from '../Thumbnails/thumbnail'
+import img from '../dist/images/railscapstone1.png'
 
 const Projects = () => {
     return(
         <div>
             <h1>Projects</h1>
+            <Thumbnail
+                link="https://freshpages.herokuapp.com/"
+                image={img}
+                title="Fresh Pages!"
+                description="Fresh pages is an entertainment website created 
+                using Ruby on Rails, based on the design idea by Nelson Sakwa on Behance"
+            />
         </div>
     )
 
