903de8020dc2dfcfe1d3b836d846d36763ae98dd
jmagero <jmagero54@gmail.com>
2021-12-03 15:39:22 -0600

    Add Skills and Contact routes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import {
 } from 'react-router-dom';
 import Projects from './components/Projects'
 import About from './components/About'
-import Articles from './components/Articles'
+import Skills from './components/Skills'
+import Contact from './components/Contact'
 import './App.css';
 import NavBar from './components/NavBar';
 
@@ -17,7 +18,8 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Projects />} />
           <Route path="/about" element={<About />} />
-          <Route path="/articles" element={<Articles />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </div>
     </Router>
