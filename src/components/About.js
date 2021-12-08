6d52598c80a1b3457bcfeb1b24b3c6a93f6b4a52
jmagero <jmagero54@gmail.com>
2021-12-08 11:35:20 -0600

    Fix issue with the see my work link

--- src/components/About.js
+++ src/components/About.js
@@ -46,7 +46,7 @@ const About = () => (
             Work With Me
           </Link>
           <Link
-            to="/projects"
+            to="/"
             className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg"
           >
             See My Past Work
