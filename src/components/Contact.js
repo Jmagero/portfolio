788eb3d44b377a41ff6879a8d3cce7bfed539481
jmagero <jmagero54@gmail.com>
2021-12-07 19:42:06 -0600

    remove mailto import

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Mailto from 'react-mailto';
+//import Mailto from 'react-mailto';
 
 export default function Contact() {
   const [name, setName] = React.useState("");
