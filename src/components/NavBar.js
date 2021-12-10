5c7ebf4d1a12119cc97c0eca42ef5679780dca06
jmagero <jmagero54@gmail.com>
2021-12-10 09:34:36 -0600

    Add background color on links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,17 +9,17 @@ const NavBar = () => (
       <Link to="/about" className="title-font font-medium text-white mb-4 md:mb-0">
         <p className="ml-3 text-xl">Jocyline</p>
       </Link>
-      <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-        <Link to="/" className="mr-5 hover:text-white">
+      <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center text-white">
+        <Link to="/" className="mr-5 hover:bg-green-600 rounded py-1 px-3">
           Past Work
         </Link>
-        <Link to="/skills" className="mr-5 hover:text-white">
+        <Link to="/skills" className="mr-5 hover:bg-green-600 rounded py-1 px-3">
           Skills
         </Link>
       </nav>
       <Link
         to="/contact"
-        className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0"
+        className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-green-700 rounded text-base mt-4 md:mt-0 text-white"
       >
         Hire Me
         <ArrowRightIcon className="w-4 h-4 ml-1" />
