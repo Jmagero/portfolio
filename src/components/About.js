2a42926f473b1951bf2933c38be55b2656fd7d3e
jmagero <jmagero54@gmail.com>
2021-12-10 09:12:51 -0600

    Add about content

--- src/components/About.js
+++ src/components/About.js
@@ -5,38 +5,21 @@ const About = () => (
   <section id="about">
     <div className="container mx-auto flex px-10 py-10 md:flex-row flex-col items-center">
       <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-        <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-          Hi, I&apos;m Jocyline.
+        <h1 className="title-font sm:text-4xl text-3xl mb-4 font-black text-grey">
+          Hello, There.
+          <br />
+          I&apos;m Jocyline.
         </h1>
         <p className="mb-10 leading-relaxed text-justify font-serif">
-          Hello! I&apos;m a full-stack developer who has graduated from Microverse,
-          a remote software development school that uses pair programming
-          and real-world projects to teach development.
+          <span className="font-bold">I&apos;m a full-stack software developer!</span>
           <br />
-          While at Microverse I spent 1300+ hours mastering
-          {' '}
-          <span className="font-bold text-blue-900 text-opacity-100">
-            algorithms,
-            data structures, and full-stack development
-            {' '}
-          </span>
-          while simultaneously
-          developing projects with
-          {' '}
-          <span className="font-bold text-blue-900 text-opacity-100"> Ruby, Rails, JavaScript, React, and Redux.</span>
+          I can help you build a product ,feature or website
           <br />
-          I have also developed skills in remote pair-programming using GitHub,
-          industry-standard git-flow,
-          and daily standups to communicate and collaborate with international
-          remote developers.
+          Look through some of my work and experience!
           <br />
-          I worked at a call centre for several years before transitioning
-          into software development. The type of work was definitely different
-          from web development, but I also learned several of the skills that
-          I’m able to use nowadays as a developer,
-          like
-          {' '}
-          <span className="font-bold text-blue-900 text-opacity-100">empathy, problem-solving, effective communication, multi-tasking, and more.</span>
+          If you like what you see and have a project you need coded,
+          <br />
+          don&apos;t hestiate to contact me.
         </p>
         <div className="flex justify-center">
           <Link
