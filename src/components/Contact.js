67cbd70db2d7b8d98239ba75f93d4f57011e5156
Jocyline Magero <jmagero54@gmail.com>
2022-03-24 14:20:57 -0500

    Add form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,53 +1,44 @@
-import React, { useState } from 'react';
+/* eslint-disable react/prop-types */
+import React from 'react';
 
-const FORM_ENDPOINT = ''; // TODO - fill on the later step
-
-const ContactForm = () => {
-  const [submitted, setSubmitted] = useState(false);
-  const handleSubmit = () => {
-    setTimeout(() => {
-      setSubmitted(true);
-    }, 100);
-  };
-
-  if (submitted) {
-    return (
-      <>
-        <div className="text-2xl">Thank you!</div>
-        <div className="text-md">I&apos;ll be in touch soon.</div>
-      </>
-    );
-  }
-
-  return (
+const ContactForm = ({
+  onSubmit, handleChange, toSend,
+}) => (
+  <section id="contact" className="bg-gray-900">
     <form
-      action={FORM_ENDPOINT}
-      onSubmit={handleSubmit}
-      method="POST"
+      className="w-80 mx-auto py-20"
+      onSubmit={onSubmit}
       target="_blank"
     >
       <div className="mb-3 pt-0">
         <input
           type="text"
-          placeholder="Your name"
-          name="name"
+          name="from_name"
+          placeholder="your name"
+          value={toSend.from_name}
+          className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div className="mb-3 pt-0">
+        <input
+          type="text"
+          name="message"
+          placeholder="Your message"
+          value={toSend.message}
+          onChange={handleChange}
           className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
           required
         />
       </div>
       <div className="mb-3 pt-0">
         <input
-          type="email"
-          placeholder="Email"
-          name="email"
-          className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
-          required
-        />
-      </div>
-      <div className="mb-3 pt-0">
-        <textarea
-          placeholder="Your message"
-          name="message"
+          type="text"
+          name="reply_to"
+          placeholder="email"
+          value={toSend.reply_to}
+          onChange={handleChange}
           className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
           required
         />
@@ -56,12 +47,13 @@ const ContactForm = () => {
         <button
           className="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
           type="submit"
+          aria-label="submit"
         >
-          Send a message
+          Get in Touch
         </button>
       </div>
     </form>
-  );
-};
+  </section>
+);
 
 export default ContactForm;
