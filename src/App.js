c8c90549b4897f5319e973d4b1029ca612c296b0
Jocyline Magero <jmagero54@gmail.com>
2022-03-24 10:54:16 -0500

    Add contact form

--- src/App.js
+++ src/App.js
@@ -3,14 +3,24 @@ import {
   Routes,
   Route,
 } from 'react-router-dom';
+import { useEffect } from 'react';
 import Projects from './components/Projects';
 import About from './components/About';
 import Skills from './components/Skills';
-import Contact from './components/Contact';
+import ContactForm from './components/Contact';
 import './App.css';
 import NavBar from './components/NavBar';
 
 function App() {
+  useEffect(() => {
+    if (document) {
+      const stylesheet = document.createElement('link');
+      stylesheet.rel = 'stylesheet';
+      stylesheet.href = 'https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css';
+
+      document.head.appendChild(stylesheet);
+    }
+  }, []);
   return (
     <Router>
       <div className="App">
@@ -19,7 +29,7 @@ function App() {
           <Route exact path="/" element={<Projects />} />
           <Route path="/about" element={<About />} />
           <Route path="/skills" element={<Skills />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="/contact" element={<ContactForm />} />
         </Routes>
       </div>
     </Router>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,48 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function Contact() {
+const FORM_ENDPOINT = ''; // TODO - fill on the later step
+
+const ContactForm = () => {
+  const [submitted, setSubmitted] = useState(false);
+  const handleSubmit = () => {
+    setTimeout(() => {
+      setSubmitted(true);
+    }, 100);
+  };
+
+  if (submitted) {
+    return (
+      <>
+        <div className="text-2xl">Thank you!</div>
+        <div className="text-md">I&apos;ll be in touch soon.</div>
+      </>
+    );
+  }
+
   return (
-    <section id="contact" className="relative">
-      <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
-        <div className="lg:w-full  md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
-          <iframe
-            width="100%"
-            height="100%"
-            title="map"
-            className="absolute inset-0"
-            frameBorder={0}
-            marginHeight={0}
-            marginWidth={0}
-            style={{ filter: 'opacity(0.7)' }}
-            src="https://www.google.com/maps/embed/v1/place?q=12100+metric+blvd&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
-          />
-        </div>
-        <div className="bg-gray-900 relative flex flex-wrap py-6 px-10 rounded shadow-md">
-          <div className="lg:w-full px-6">
-            <h2 className="title-font font-semibold text-white tracking-widest text-xs">
-              ADDRESS
-            </h2>
-            <p className="mt-1 text-white">
-              Austin
-              {' '}
-              <br />
-              Texas
-            </p>
-          </div>
-          <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
-            <h2 className="title-font font-semibold text-white tracking-widest text-xs">
-              EMAIL
-            </h2>
-            <p className="text-white leading-relaxed">
-              jmagero54@gmail.com
-            </p>
-            <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
-              PHONE
-            </h2>
-            <p className="text-white">+(737)2962902</p>
-          </div>
-        </div>
+    <form
+      action={FORM_ENDPOINT}
+      onSubmit={handleSubmit}
+      method="POST"
+      target="_blank"
+    >
+      <div className="mb-3 pt-0">
+        <input
+          type="text"
+          placeholder="Your name"
+          name="name"
+          className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+          required
+        />
       </div>
-    </section>
+      <div className="mb-3 pt-0">
+        <input
+          type="email"
+          placeholder="Email"
+          name="email"
+          className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+          required
+        />
+      </div>
+      <div className="mb-3 pt-0">
+        <textarea
+          placeholder="Your message"
+          name="message"
+          className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+          required
+        />
+      </div>
+      <div className="mb-3 pt-0">
+        <button
+          className="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+          type="submit"
+        >
+          Send a message
+        </button>
+      </div>
+    </form>
   );
-}
+};
+
+export default ContactForm;
