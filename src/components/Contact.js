88cdb342da57bc17b2dbfa93ab9a55733f0b1dcd
Jocyline Magero <jmagero54@gmail.com>
2022-03-24 14:26:23 -0500

    Change message input type to textarea

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -24,17 +24,6 @@ const ContactForm = ({
       <div className="mb-3 pt-0">
         <input
           type="text"
-          name="message"
-          placeholder="Your message"
-          value={toSend.message}
-          onChange={handleChange}
-          className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
-          required
-        />
-      </div>
-      <div className="mb-3 pt-0">
-        <input
-          type="text"
           name="reply_to"
           placeholder="email"
           value={toSend.reply_to}
@@ -43,6 +32,18 @@ const ContactForm = ({
           required
         />
       </div>
+      <div className="mb-3 pt-0">
+        <textarea
+          type="textarea"
+          name="message"
+          placeholder="Your message"
+          value={toSend.message}
+          onChange={handleChange}
+          className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+          required
+        />
+      </div>
+
       <div className="mb-3 pt-0">
         <button
           className="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
