ce740f553183b70552f2226ed4d775a66d53b802
Jocyline Magero <jmagero54@gmail.com>
2022-03-24 10:53:48 -0500

    restyle the about section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        about: "url('./flame-move.png')",
+      },
+    },
   },
   variants: {
     extend: {},
