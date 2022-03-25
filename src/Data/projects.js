78cf7190ae16c95058d22ea4fdd7e06452f5569a
Jocyline Magero <jmagero54@gmail.com>
2022-03-25 11:09:53 -0500

    Add github links

--- src/Data/projects.js
+++ src/Data/projects.js
@@ -27,6 +27,7 @@ const projects = [
       'This simple To Do list web page is built using webpack and served by a webpack dev server',
     image: './to-do-list.png',
     link: 'https://musing-hugle-2acbcb.netlify.app/',
+    source: 'https://github.com/Jmagero/To-do-list2',
   },
   {
     title: 'BookStore',
@@ -36,6 +37,7 @@ const projects = [
       'This project is about laying the foundations for Bookstore website',
     image: './bookstore.png',
     link: 'https://competent-jennings-d66a97.netlify.app/',
+    source: 'https://github.com/Jmagero/bookstore',
   },
 ];
 
