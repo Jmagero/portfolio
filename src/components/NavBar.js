c05e88a3083996a38092e0eff20fe80d0e02fe1f
Jocyline Magero <jmagero54@gmail.com>
2022-04-05 10:49:52 -0500

    Correct the links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,11 +6,11 @@ import { Link } from 'react-router-dom';
 const NavBar = () => (
   <header className="bg-gray-800 md:sticky top-0 z-10">
     <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-      <Link to="/about" className="title-font font-medium text-white mb-4 md:mb-0">
+      <Link to="/" className="title-font font-medium text-white mb-4 md:mb-0">
         <p className="ml-3 text-xl">Jocyline</p>
       </Link>
       <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center text-white">
-        <Link to="/" className="mr-5 hover:bg-green-600 rounded py-1 px-3">
+        <Link to="/projects" className="mr-5 hover:bg-green-600 rounded py-1 px-3">
           Past Work
         </Link>
         <Link to="/skills" className="mr-5 hover:bg-green-600 rounded py-1 px-3">
